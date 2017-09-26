5e6d3ee164fefd92108b75f49e44c1b80d610a9a
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-26 19:46:38 +0000

    added stylization to output of query

--- lookup_people.js
+++ lookup_people.js
@@ -3,23 +3,36 @@ const settings = require("./settings"); // settings.json
 const personsName = process.argv.slice(2)[0];
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
 });
- 
-client.connect((err) => {
+
+client.connect(err => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [personsName], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
+  console.log("Searching ...");
+  client.query(
+    "SELECT  id, first_name, last_name, to_char(birthdate,'YYYY-MM-DD') as birthdate FROM famous_people WHERE first_name = $1::text OR last_name = $1::text",
+    [personsName],
+    (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+
+      const queryOutput = result.rows;
+
+      console.log(`Found ${queryOutput.length} person(s) by the name '${personsName}':`);
+
+      for (let i = 0; i < queryOutput.length; i++) {
+        console.log(`- ${i + 1}: ${queryOutput[i].first_name} ${queryOutput[i].last_name}, born '${queryOutput[i].birthdate}'`);
+      }
+
+      client.end();
     }
-    console.log(result.rows[0]);
-    client.end();
-  });
+  );
 });
