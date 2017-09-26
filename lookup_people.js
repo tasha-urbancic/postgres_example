24b300ce7d5d088e9dbf3b63528568e6d3109dbb
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-26 19:23:47 +0000

    added query to lookup famous people by first or last name from the command line

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
+const personsName = process.argv.slice(2)[0];
 
 const client = new pg.Client({
   user     : settings.user,
@@ -9,16 +10,16 @@ const client = new pg.Client({
   port     : settings.port,
   ssl      : settings.ssl
 });
-
+ 
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [personsName], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
+    console.log(result.rows[0]);
     client.end();
   });
 });
